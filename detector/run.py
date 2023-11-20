f2fc4d567a1b237fb47c087da97c94898851ad0a
antirez <antirez@gmail.com>
2023-11-20 11:40:01 +0100

    Write box only for the max confidence found.

--- detector/run.py
+++ detector/run.py
@@ -45,6 +45,10 @@ def show_matching_boxes(boxes, confs, image_width=416, image_height=416, thresho
             # Print the adjusted bounding box and its confidence score
             print(f"Box: {x_min_pixel}, {y_min_pixel}, {x_max_pixel}, {y_max_pixel}, Confidence: {conf}")
 
+# Draw a box on the area with the maxium confidence score. Take the
+# orignal image path and the output path of the new processed image.
+# The threshold argument is used to write no box at all if the confidence
+# is less than the specified amount.
 def draw_boxes(image_path, output_path, boxes, confidences, threshold=0.1):
     # Load the image
     image = Image.open(image_path)
@@ -57,6 +61,9 @@ def draw_boxes(image_path, output_path, boxes, confidences, threshold=0.1):
         font = ImageFont.load_default()
 
     # Iterate over the bounding boxes and confidences
+    # Find the best one, if any
+    max_box = None
+    max_confidence = None
     for box, confidence in zip(boxes, confidences):
         confidence = confidence[0]
         if confidence < threshold: continue
@@ -66,13 +73,17 @@ def draw_boxes(image_path, output_path, boxes, confidences, threshold=0.1):
         y_min *= image.height
         x_max *= image.width
         y_max *= image.height
+        if max_confidence == None or max_confidence < confidence:
+            max_confidence = confidence
+            max_box = [x_min,y_min,x_max,y_max]
 
-        # Draw the bounding box
-        draw.rectangle([x_min, y_min, x_max, y_max], outline="red", width=2)
+    # Draw the bounding box
+    if max_confidence:
+        draw.rectangle(max_box, outline="red", width=2)
 
         # Draw the confidence score
-        score_text = f"{confidence:.2f}"
-        draw.text((x_min, y_min), score_text, fill="red", font=font)
+        score_text = f"{max_confidence:.2f}"
+        draw.text((max_box[0],max_box[1]), score_text, fill="red", font=font)
 
     # Write the current time as well.
     draw.text((20,20), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), fill="green", font=font)
